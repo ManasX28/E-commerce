cd746cbde3e86b48a00664c191f1135d76226bdd
Manas Agarwal <manas.cs18@bmsce.ac.in>
2020-06-14 06:54:00 +0530

    added the feature to add items in the cart

--- buy-it/src/components/collection-preview-component/collection-preview.component.jsx
+++ buy-it/src/components/collection-preview-component/collection-preview.component.jsx
@@ -9,8 +9,8 @@ const CollectionPreview = ({title, items}) => (
             {
                 items
                 .filter((item) => item.id < 5)
-                .map(({id, ...otherItemProps}) =>  (
-                    <CollectionItem key={id} {...otherItemProps} />
+                .map((item) =>  (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
